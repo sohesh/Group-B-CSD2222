b4cfac2623e1520830e15c5ff66d0109cf4194a7
Paymon M <paymonm@hotmail.co.uk>
2014-11-05 14:10:33 +0000

    Yo!

---
+++ src/testStudent.java
@@ -0,0 +1,11 @@
+
+public class testStudent {
+	public static void main(String[] args) {
+		
+		Student firstStudent = new Student("Paymon", 19, "Computer Science");
+		
+		
+		firstStudent.displayStudentDetails();
+	}
+
+}
